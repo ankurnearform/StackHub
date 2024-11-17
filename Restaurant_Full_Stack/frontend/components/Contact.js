c74c91ffa803885399d031a8616f009668d4f43b
ankurnearform <ankur.chopra@nearform.com>
2024-11-17 12:13:14 -0500

    Add generated Changes based on JIRA https://genaiprojects.atlassian.net/browse/SCRUM-5

---
+++ Restaurant_Full_Stack/frontend/components/Contact.js
@@ -0,0 +1,11 @@
+const Contact = () => {
+  return (
+    <div>
+      <h2>Contact Us</h2>
+      <p>Phone: (123) 456-7890</p>
+      <p>Email: contact@restaurant.com</p>
+    </div>
+  );
+};
+
+export default Contact;
